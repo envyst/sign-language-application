61d4355b15968cb7938259f8081f09e56c91a512
Nur Ardli Rachmat Saputra <C2292226@bangkit.academy>
2021-05-26 16:49:28 +0700

    add mobile app to folder mobile-dev-file

---
+++ mobile-dev-file/app/src/main/java/com/example/androidapp/SplashScreenActivity.kt
@@ -0,0 +1,19 @@
+package com.example.androidapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 1500)
+    }
+}
